c27270b4cabf790a5f81f318afc2ce5b9b17855e
James B. Matthews <james30152@gmail.com>
2018-03-18 15:20:14 -0400

    +add

--- ansible-bootstrap/env-init.sh
+++ ansible-bootstrap/env-init.sh
@@ -1,29 +1,13 @@
 #General
 apt install update
-
-# Git 
-# Install
 apt-get install -y git 
-# Clone
-git clone https://github.com/JBMatthews/ansible-lab-setup.git
-# Copy payload to 'ansible' node
-# cp ./ansible-lab-setup 
-
-# Shared Volumes
 mkdir shared_volume  
 docker pull williamyeh/ansible:ubuntu14.04-onbuild  
 docker pull ubuntu:14.04  
 docker network create mynetwork  
 cd shared_volume 
-
-cp ../ansible-lab-setup .
-
-
-# Launch Containers
 docker run --rm williamyeh/ansible:ubuntu14.04-onbuild echo hello!  
 docker run --rm ubuntu:14.04 echo hello!  
-# Ansible
 docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
-#Target
 docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
   
