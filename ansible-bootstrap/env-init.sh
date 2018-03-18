052de95ed7ed217944d4ccc25c86fcb202cb2e41
James B. Matthews <jamesbematthews@gmail.com>
2018-03-18 18:45:24 -0400

    Update env-init.sh

--- ansible-bootstrap/env-init.sh
+++ ansible-bootstrap/env-init.sh
@@ -11,8 +11,3 @@ docker run --rm williamyeh/ansible:ubuntu14.04-onbuild echo hallo
 docker run --rm ubuntu:14.04 echo hallo  
 docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume williamyeh/ansible:ubuntu14.04-onbuild /bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
 docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
-
-# Usage:  
-# docker cp [OPTIONS] CONTAINER:SRC_PATH DEST_PATH|-
-# docker cp [OPTIONS] SRC_PATH|- CONTAINER:DEST_PATH [flags]
-docker cp /home/scrapbook/tutorial/ansible-lab-setup|- ansible:/home
