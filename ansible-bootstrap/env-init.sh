32bbcbce97026856189736dadbd4228375d28013
James B. Matthews <james30152@gmail.com>
2018-03-22 20:03:41 -0400

    +add

--- ansible-bootstrap/env-init.sh
+++ ansible-bootstrap/env-init.sh
@@ -1,12 +1,12 @@
 apt-get -y update
 apt install -y git
 cd /home/scrapbook/tutorial
-mkdir shared_volume  
-docker pull virtuant/ansible-node:v2  
-docker pull ubuntu:14.04  
-docker network create mynetwork  
+mkdir shared_volume
+docker pull virtuant/ansible-node:v2
+docker pull ubuntu:14.04
+docker network create mynetwork
 cd shared_volume
-docker run --rm virtuant/ansible-node:v2 echo hello-world  
-docker run --rm ubuntu:14.04 echo hello-world  
-docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume virtuant/ansible-node:v2 bin/bash -c 'while true; do sleep 60; echo keepalive; done'  
-docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'  
+docker run --rm virtuant/ansible-node:v2 echo hello-world
+docker run --rm ubuntu:14.04 echo hello-world
+docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume virtuant/ansible-node:v2 bin/bash -c 'while true; do sleep 60; echo keepalive; done'
+docker run -d --name target --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 /bin/bash -c 'apt-get update; apt-get install -y openssh-server;service ssh start; while true; do sleep 60; echo keepalive; done'
