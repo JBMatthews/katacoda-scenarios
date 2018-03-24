9696f16e2a7362d621ce1736fe201d523ab901e6
James B. Matthews <james30152@gmail.com>
2018-03-24 13:04:24 -0400

    add

--- ansible-bootstrap/env-init.sh
+++ ansible-bootstrap/env-init.sh
@@ -10,5 +10,7 @@ docker pull virtuant/ansible-node:v2
 docker pull ubuntu:14.04
 docker network create mynetwork
 cd shared_volume
-docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume virtuant/ansible-node:v2
-docker run -d --name target -p 8080 --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04
+docker run -d --name ansible --rm --network mynetwork -v $(pwd):/shared_volume virtuant/ansible-node:v2 bash -c 'while true; do sleep 60; echo keepalive; done' 
+docker run -d --name target -p 8080 --rm --network mynetwork -v $(pwd):/shared_volume ubuntu:14.04 bash -c 'apt-get update; apt-get install -y openssh-server; service ssh start; while true; do sleep 60; echo keepalive; done'  
+
+
